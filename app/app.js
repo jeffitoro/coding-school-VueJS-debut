7fa1957e31d2951788bc18d037f901a2ff5715ce
Jeffito <jeff-oro@hotmail.com>
2017-09-21 14:27:49 +0200

    fonctionnalité edition task

--- app/app.js
+++ app/app.js
@@ -1,13 +1,29 @@
 Vue.component("add-task", {
     props: ["taskprop"],
-    template: '<li class="list-group-item"><span id="txt">{{taskprop.txt}}</span><button v-on:click="addDone" class="pull-right"><i class="fa fa-check-square-o" aria-hidden="true"></i></button><button class="pull-right"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button><button class="pull-right"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>',
+    template: '<li class="list-group-item" style="max-height:41px"><form v-on:submit.prevent="editOk" v-if="editActive" style="vertical-align:middle;display:inline-block;width:auto;max-height:41px"><input v-on:focus="focusActive" v-bind:class="{error: isActive}" v-model="taskprop.txt" type="text" class="form-control"style="vertical-align:middle;display:inline-block;width:auto;height:19px"><button type="submit" class="btn btn-primary" style="vertical-align:middle">Ok</button></form><span v-else>{{taskprop.txt}}</span><button v-on:click="addDone" class="pull-right"><i class="fa fa-check-square-o" aria-hidden="true"></i></button><button v-on:click="addUpdtask" class="pull-right"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button><button class="pull-right"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>',
+    data: function () {
+        return { editActive: false, isActive: false }
+    },
     methods: {
         addDone: function () {
             this.$el.classList.toggle("done");
+        },
+        addUpdtask: function () {
+            this.editActive = true;
+        },
+        editOk: function () {
+            if (this.$props.taskprop.txt == "") {
+                this.isActive = true;
+                document.activeElement.blur();
+            } else {
+                this.editActive = false;
+            }
+        },
+        focusActive: function () {
+            this.isActive = false;
         }
     }
 })
-
 let app = new Vue({
     el: "#app",
     created: function () {
--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -80,14 +80,30 @@ module.exports = __webpack_require__(2);
 
 Vue.component("add-task", {
     props: ["taskprop"],
-    template: '<li class="list-group-item"><span id="txt">{{taskprop.txt}}</span><button v-on:click="addDone" class="pull-right"><i class="fa fa-check-square-o" aria-hidden="true"></i></button><button class="pull-right"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button><button class="pull-right"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>',
+    template: '<li class="list-group-item" style="max-height:41px"><form v-on:submit.prevent="editOk" v-if="editActive" style="vertical-align:middle;display:inline-block;width:auto;max-height:41px"><input v-on:focus="focusActive" v-bind:class="{error: isActive}" v-model="taskprop.txt" type="text" class="form-control"style="vertical-align:middle;display:inline-block;width:auto;height:19px"><button type="submit" class="btn btn-primary" style="vertical-align:middle">Ok</button></form><span v-else>{{taskprop.txt}}</span><button v-on:click="addDone" class="pull-right"><i class="fa fa-check-square-o" aria-hidden="true"></i></button><button v-on:click="addUpdtask" class="pull-right"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button><button class="pull-right"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>',
+    data: function data() {
+        return { editActive: false, isActive: false };
+    },
     methods: {
         addDone: function addDone() {
             this.$el.classList.toggle("done");
+        },
+        addUpdtask: function addUpdtask() {
+            this.editActive = true;
+        },
+        editOk: function editOk() {
+            if (this.$props.taskprop.txt == "") {
+                this.isActive = true;
+                document.activeElement.blur();
+            } else {
+                this.editActive = false;
+            }
+        },
+        focusActive: function focusActive() {
+            this.isActive = false;
         }
     }
 });
-
 var app = new Vue({
     el: "#app",
     created: function created() {
