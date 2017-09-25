35ced4d021732297c82090fabd7c60b33ce95b43
Jeffito <jeff-oro@hotmail.com>
2017-09-25 15:48:17 +0200

    animation titre et button remove task

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import Vue from "../node_modules/vue/dist/vue";
 Vue.component("add-task", {
     props: ["taskprop"],
-    template: '<li class="list-group-item" style="max-height:41px" v-bind:class="taskprop.status"><form v-on:submit.prevent="editOk" v-if="editActive" style="vertical-align:middle;display:inline-block;width:auto;max-height:41px"><input v-on:focus="focusActive" v-bind:class="{error: isActive}" v-model="taskprop.txt" type="text" class="form-control"style="vertical-align:middle;display:inline-block;width:auto;height:19px"><button type="submit" class="btn btn-primary" style="vertical-align:middle">Ok</button></form><span v-else>{{taskprop.txt}}</span><button v-on:click="addDone" class="pull-right"><i class="fa fa-check-square-o" aria-hidden="true"></i></button><button v-on:click="addUpdtask" class="pull-right"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button><button v-on:click="deletetask" class="pull-right"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>',
+    template: '<li class="list-group-item" style="max-height:41px" v-bind:class="taskprop.status" v-on:animationend="deleteAnimation"><form v-on:submit.prevent="editOk" v-if="editActive" style="vertical-align:middle;display:inline-block;width:auto;max-height:41px"><input v-on:focus="focusActive" v-bind:class="{error: isActive}" v-model="taskprop.txt" type="text" class="form-control"style="vertical-align:middle;display:inline-block;width:auto;height:19px"><button type="submit" class="btn btn-primary" style="vertical-align:middle">Ok</button></form><span v-else>{{taskprop.txt}}</span><button v-on:click="addDone" class="pull-right"><i class="fa fa-check-square-o" aria-hidden="true"></i></button><button v-on:click="addUpdtask" class="pull-right"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button><button v-on:click="deletetask" class="pull-right"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>',
     data: function () {
         return { editActive: false, isActive: false }
     },
@@ -25,6 +25,9 @@ Vue.component("add-task", {
             this.isActive = false;
         },
         deletetask() {
+            this.$el.classList.add("animated", "fadeOutLeftBig");
+        },
+        deleteAnimation() {
             this.$el.parentElement.removeChild(this.$el);
         }
     }
@@ -46,7 +49,8 @@ let app = new Vue({
         task: "",
         isActive: false,
         tasks: [],
-        filter: "all"
+        filter: "all",
+        isMouseOver: false
     },
     methods: {
         submitTask: function () {
@@ -64,6 +68,12 @@ let app = new Vue({
         },
         clickFilter(event) {
             this.$data.filter = event.toElement.textContent.toLowerCase();
+        },
+        spanAnimation() {
+            this.$el.querySelector("h1>span").classList.add("animated", "hinge", "flip");
+        },
+        spanExitAnimation() {
+            this.$el.querySelector("h1>span").classList.remove("animated", "hinge", "flip");
         }
     }
 })
